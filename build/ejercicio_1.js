4571bba8340c7d72e342bbc5293ca1914df6aef0
Felipe Mosquera <felipe298@gmail.com>
2024-04-15 21:34:44 +0200

    Ejercicio 4 Resuelto

--- build/ejercicio_1.js
+++ build/ejercicio_1.js
@@ -1,13 +1,14 @@
 "use strict";
 var _a;
-var button = (_a = document.querySelector(".next")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", randomJoke);
+var button = (_a = document
+    .querySelector(".next")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", randomJoke);
 function randomJoke() {
     let config = {
         headers: {
             Accept: "application/json",
-        }
+        },
     };
-    let jokes = fetch('https://icanhazdadjoke.com/', config)
+    let jokes = fetch("https://icanhazdadjoke.com/", config)
         .then((res) => res.json())
         .then((data) => {
         const joke = data.joke;
@@ -22,20 +23,44 @@ function randomJoke() {
 randomJoke();
 let reportAcudits = [];
 function scoreJokes() {
-    let scoreSelect = document.querySelector('#score');
+    let scoreSelect = document.querySelector("#score");
     let score = parseInt(scoreSelect.value);
-    randomJoke().then((joke) => {
-        console.log('linea 42', joke);
+    randomJoke()
+        .then((joke) => {
         let newJokes = {
             joke: joke,
             score: score,
-            date: new Date().toISOString()
+            date: new Date().toISOString(),
         };
         reportAcudits.push(newJokes);
-        console.log('linea de nuevos chistes', reportAcudits);
     })
         .catch((error) => {
-        console.error('Error getting joke:', error);
+        console.error("Error getting joke:", error);
     });
 }
 scoreJokes();
+/*--------------------------------------------------Ejercicio #4------------------------------------------------------------------------------*/
+//funcion para convertir la temperatura kelvin celsius
+function kelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+}
+function weatherAPI() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=${API_Key}2`)
+        .then((res) => res.json())
+        .then((data) => {
+        const weathercity = (data.name = "Barcelona");
+        const iconName = data.weather[0].icon;
+        const weather = kelvinToCelsius(data.main.temp).toFixed(2);
+        const description = data.weather[0].description;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
+        const weather_box = document.getElementById("wheater_container");
+        weather_box.innerHTML = `
+        <img src="${iconUrl}" alt="tempeture_code">
+        <h1>${weathercity}</h1>
+        <h3>${weather} ºC</h3>
+        <h2>${description}</h2>
+        `;
+        console.log(data);
+    });
+}
+weatherAPI();
--- ejercicio_1.ts
+++ ejercicio_1.ts
@@ -1,62 +1,91 @@
-var button = document.querySelector(".next")?.addEventListener("click", randomJoke);
+var button = document
+  .querySelector(".next")
+  ?.addEventListener("click", randomJoke);
 
+function randomJoke() {
+  let config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
 
-function randomJoke(){
-    let config = {
-        headers: {
-            Accept: "application/json",
-        }
-    };
-
-    let jokes = fetch('https://icanhazdadjoke.com/', config)
+  let jokes = fetch("https://icanhazdadjoke.com/", config)
     .then((res) => res.json())
     .then((data) => {
-        const joke = data.joke;
-        console.log(joke)
-        const randomJoke = document.querySelector(".jokes")
-        if(randomJoke){
-            randomJoke.innerHTML = joke
-        }
-    
-})
+      const joke = data.joke;
+      console.log(joke);
+      const randomJoke = document.querySelector(".jokes");
+      if (randomJoke) {
+        randomJoke.innerHTML = joke;
+      }
+    });
 
-return jokes
+  return jokes;
 }
 
-randomJoke()
+randomJoke();
 
 /*--------------------------------------------------Ejercicio #3------------------------------------------------------------------------------*/
 
 type Jokes = {
-    joke: any,
-    score: number,
-    date: string,
+  joke: any;
+  score: number;
+  date: string;
 };
 
-let reportAcudits: Jokes[]  = []
-
+let reportAcudits: Jokes[] = [];
 
 function scoreJokes() {
-    let scoreSelect = document.querySelector('#score') as HTMLSelectElement;
-    let score = parseInt(scoreSelect.value)
+  let scoreSelect = document.querySelector("#score") as HTMLSelectElement;
+  let score = parseInt(scoreSelect.value);
 
-    randomJoke().then((joke) => {
-            console.log('linea 42', joke);
+  randomJoke()
+    .then((joke) => {
+      let newJokes: Jokes = {
+        joke: joke,
+        score: score,
+        date: new Date().toISOString(),
+      };
 
-            let newJokes: Jokes = {
-                joke: joke,
-                score: score,
-                date: new Date().toISOString()
-            };
-
-            reportAcudits.push(newJokes);
-
-            console.log('linea de nuevos chistes', reportAcudits);
-        })
-        .catch((error) => {
-            console.error('Error getting joke:', error);
-        });
-   
+      reportAcudits.push(newJokes);
+    })
+    .catch((error) => {
+      console.error("Error getting joke:", error);
+    });
 }
 
-scoreJokes()
+scoreJokes();
+
+/*--------------------------------------------------Ejercicio #4------------------------------------------------------------------------------*/
+//funcion para convertir la temperatura kelvin celsius
+function kelvinToCelsius(kelvin : number) {
+    return kelvin - 273.15;
+}
+
+
+function weatherAPI() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=${API_Key}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const weathercity : string = (data.name = "Barcelona");
+      const iconName : string = data.weather[0].icon;
+      const weather : number = kelvinToCelsius(data.main.temp).toFixed(2);
+      const description : string = data.weather[0].description;
+      const iconUrl = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
+      const weather_box = document.getElementById("wheater_container");
+      weather_box.innerHTML = `
+        <img src="${iconUrl}" alt="tempeture_code">
+        <h1>${weathercity}</h1>
+        <h3>${weather} ºC</h3>
+        <h2>${description}</h2>
+        `;
+
+        console.log(data)
+    });
+
+    
+}
+
+weatherAPI()
